18de89c11ce05a1ec33a4fe0886f013ca3083b42
jajcter <59999133+jajcter@users.noreply.github.com>
2020-01-17 12:48:58 -0500

    gamer_fest
    proyecto de ing web

---
+++ gamer_fest/views/site/about.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+    </p>
+    <dir video controlslist="nodownload"><iframe width="80%" height="315;" src="https://www.youtube.com/embed/fuQLB3hQfHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></dir>
+
+    <code><?= __FILE__ ?></code>
+</div>
---
+++ gamer_fest/views/site/index.php
@@ -0,0 +1,59 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Gamer Fest';
+?>
+<div class="site-index" style="background-image: url('../img/f_2.png'); background-size: 100% 100%;  background-attachment: fixed;">
+
+    <div class="jumbotron" >
+        <!--<h1>GAMER FEST 20/20!</h1>-->
+        <dir video controlslist="nodownload"><iframe width="80%" height="315;" src="https://www.youtube.com/embed/fuQLB3hQfHw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></dir>
+
+        <div style="background-color:rgb(0,0,0);opacity: 0.5;position: center;"><h1 style="color: white;">Juega Diviertete y Gasta</h1></div>
+
+        <p ><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Inscribete Ya!</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row" bgcolor="red" >
+            <div class="col-lg-4" bgcolor="red"style="background-color: white;height: 180px; opacity: 0.9;">
+                <h2>Juegos</h2>
+                
+                <ul>
+
+                    <li> DOTA</li>
+                    <li> FIFA 2020 </li>
+                    <li> MARIO KART </li>
+                    <li> LOL </li>
+                    
+                </ul>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Y MAS... &raquo;</a></p>
+            </div>
+            <div class="col-lg-4" bgcolor="red" style="background-color: white;height: 180px;  opacity: 0.9;">
+                <h2>Anuncios</h2>
+
+                <ul>
+
+                    <li> Fechas</li>
+                    <li> Auspiciantes </li>
+                    <p></p>
+                    <p></p>
+
+                </ul>
+
+                <p><a class="btn btn-default" href="index.php?r=site%2Fabout">Y MAS... &raquo;</a></p>
+            </div>
+            <div class="col-lg-4" bgcolor="red" style="background-color: white; height: 180px; opacity: 0.9;">
+                <h2>Partidas</h2>
+
+                <p>Debes estar pendiente a todos los juegos en los que estas inscritos.</p>
+                <p>Estaremos anunciando las partidas Proximamente.</p>
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Y MAS... &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
